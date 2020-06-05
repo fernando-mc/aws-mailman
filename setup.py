846d03933969d01a776b4802324181f356062e5b
Fernando Medina Corey <fmcorey@gmail.com>
2020-06-05 15:43:33 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from distutils.core import setup
+import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='awsmailman',
     packages=['awsmailman'],
-    version='0.2.3',
+    version='0.2.4',
     license='MIT',
     description='A utility for updating domain registrant information in Amazon Route 53',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Fernando Medina Corey',
     author_email='fernandomc.sea@gmail.com',
-    url='https://github.com/fernando-mc/',
-    download_url='https://github.com/fernando-mc/aws-mailman/archive/v0.2.3.tar.gz',
+    url='https://github.com/fernando-mc/aws-mailman',
     keywords=['AWS', 'Route53', 'Domains'],
     entry_points={
         'console_scripts': [
